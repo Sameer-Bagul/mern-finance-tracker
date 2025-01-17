69d84ccb8bab233128b4b75c73e42ca187d562be
sameer <sameerbagul2004@gmail.com>
2025-01-17 09:12:47 +0530

    server done

---
+++ client/src/pages/dashboard/financial-record-list.tsx
@@ -0,0 +1,6 @@
+export const FinancialRecordList = () => {
+    
+    return (
+        <div className="">Record List</div>    
+    )
+}
---
+++ client/src/pages/dashboard/index.tsx
@@ -0,0 +1,16 @@
+import { useUser } from "@clerk/clerk-react";
+import { FinancialRecordForm } from "./financial-record-form";
+import { FinancialRecordList } from "./financial-record-list";
+import "./financial-record.css"
+export const Dashboard = () => {
+  
+  const { user } = useUser();
+
+  return (
+    <div className="dashboard-container">
+      <h1 className="">Welcome {user?.firstName}! Here Are Your Finances</h1>
+      <FinancialRecordForm />
+      <FinancialRecordList />
+    </div>
+  );
+};
